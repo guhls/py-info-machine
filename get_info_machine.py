d7dba2197001a6bb2917a3a5063405d5e4b57ef8
guhls <78971590+guhls@users.noreply.github.com>
2022-05-15 22:52:52 -0300

    Refactor code to send csv to server

--- get_info_machine.py
+++ get_info_machine.py
@@ -1,43 +1,72 @@
 import os, platform, psutil, socket
-import re
+import tqdm
 import pandas as pd
 
 from demo_minio import push_to_minio
+
 
 def main():
     os.system("cls")
 
     my_system = platform.uname()
-
-    print(my_system.node)
-
     memory = list(psutil.virtual_memory())
 
-    df = pd.DataFrame([[os.getlogin(), f"{my_system.system} {my_system.release}", str(memory[0])[:2]]], columns=["User", "Sys", "RAM"])
-    print(df)
+    df = pd.DataFrame(
+        [
+            [
+                os.getlogin(),
+                f"{my_system.system} {my_system.release}",
+                str(memory[0])[:2],
+            ]
+        ],
+        columns=["User", "Sys", "RAM"],
+    )
 
-    # with open("gustavopc-infosys.csv", 'r') as file:
-    #     print(pd.read_csv(file))
+    filename = f"archive\{my_system.node}-infosys.csv"
 
-    #push_to_minio(df)
+    df.to_csv(f"{filename}", index=False)
 
-    #df.to_csv(f"archive\{my_system.node}-infosys.csv", index=False)
-
-    return df
+    return filename
 
 
 def send_df_to_server():
-    df = main()
+    SEPARATOR = ","
+    BUFFER_SIZE = 4096
 
-    HOST = '127.0.0.1'
+    filename = main()
+    filesize = os.path.getsize(filename)
+
+    HOST = "127.0.0.1"
     PORT = 50000
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
-    s.sendall(df.to_csv(index=False).encode())
-    data = s.recv(1024)
 
-    print("Msg ecoada", data.decode())
+    s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+
+    progress = tqdm.tqdm(
+        range(filesize),
+        f"Sending {filename}",
+        unit="B",
+        unit_scale=True,
+        unit_divisor=1024,
+    )
+
+    with open(filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in
+            # busy networks
+            s.sendall(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+
+    # close the socket
+    s.close()
 
 
 if __name__ == "__main__":
--- server-client/server.py
+++ server-client/server.py
@@ -1,25 +1,52 @@
-import socket
-import pandas as pd
+import socket, tqdm, os
 
-HOST = 'localhost'
+
+HOST = "localhost"
 PORT = 50000
+
+BUFFER_SIZE = 4096
+SEPARATOR = ","
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-s.listen()
+s.listen(5)
 print("Aguardando ...")
-conn, ender = s.accept()
+conn, address = s.accept()
 
-print("conectado em", ender)
-while True:
-    data = conn.recv(1024)
+# receive the file infos
+# receive using client socket, not server socket
+received = conn.recv(BUFFER_SIZE).decode()
+filename, filesize = received.split(SEPARATOR)
+# remove absolute path if there is
+filename = os.path.basename(filename)
+# convert to integer
+filesize = int(filesize)
 
-    from io import BytesIO
+print("conectado em", address)
 
-    pd.read_csv(BytesIO(data)).to_csv("archive/" ,index=False)
+# start receiving the file from the socket
+# and writing to the file stream
+progress = tqdm.tqdm(
+    range(filesize),
+    f"Receiving {filename}",
+    unit="B",
+    unit_scale=True,
+    unit_divisor=1024,
+)
+with open(f"archive\{filename}", "wb") as f:
+    while True:
+        # read 1024 bytes from the socket (receive)
+        bytes_read = conn.recv(BUFFER_SIZE)
+        if not bytes_read:
+            # nothing is received
+            # file transmitting is done
+            break
+        # write to the file the bytes we just received
+        f.write(bytes_read)
+        # update the progress bar
+        progress.update(len(bytes_read))
 
-    if not data:
-        print("Closing conn")
-        conn.close()
-        break
-    conn.sendall(data)
+# close the client socket
+conn.close()
+# close the server socket
+s.close()
